69a8227684333dbdcf7db7f19945f5f0426eceed
Rafi Khan <zetoslab@gmail.com>
2018-07-11 16:24:44 -0400

    Added basic banner home page
    There is still a bit of work to do, but so far it displays some
    information about the project and has a navigation bar.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
-import CircularProgress from '@material-ui/core/CircularProgress';
-
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import AppBar from '../ui/AppBar.js';
+import Banner from '../ui/Banner.js';
 
 const theme = createMuiTheme();
 
 export default class App extends Component {
     render() {
+	const technologies = [
+	    "MeteorJS",
+	    "ReactJS",
+	    "React Redux",
+	    "React Router",
+	    "MongoDB",
+	    "Material-UI",
+	    "Uniforms"
+	];
+
 	return (
 	    <MuiThemeProvider theme={theme}>
-	      <p>Some text</p>
-	      <CircularProgress/>
+	      <AppBar/>
+	      <Banner theme={theme}
+		      technologies={technologies}/>
 	    </MuiThemeProvider>
 	);
     }
---
+++ imports/ui/Banner.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const styles = theme => ({
+  root: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 4,
+      margin: theme.spacing.unit * 4,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+
+class Banner extends Component {
+    constructor (props) {
+	super(props);
+    }
+
+    render() {
+
+	const { classes } = this.props;
+
+	return (
+	    <div className={classes.root}>
+	      <Grid container spacing={24}>
+		<Grid item xs={12}>
+		  <Paper className={classes.paper}>
+		    <Typography color="inherit" variant="display3">
+		      Welcome to Simple Contact
+		    </Typography>
+		  </Paper>
+		</Grid>
+
+		<Grid item xs={12}>
+		  <Paper className={classes.paper}>
+		    <Typography color="inherit" variant="subheading">
+		      This is an application developed using the following modern web technologies.
+		    </Typography>
+		    <List>
+		      {
+			  this.props.technologies.map( (tech) => {
+			      return (
+				  <ListItem key={tech}>
+				    <ListItemText>
+				      {tech}
+				    </ListItemText>
+				  </ListItem>
+			      );
+			  })
+		      }
+		       </List>
+		  </Paper>
+		</Grid>
+
+	      </Grid>
+	    </div>
+	);
+    }
+}
+
+export default withStyles(styles)(Banner);

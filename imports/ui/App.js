eaf641d83264557bb62fba3bedc7858d0781ea63
Rafi Khan <zetoslab@gmail.com>
2018-07-11 14:56:34 -0400

    Created App Component
    Successfully rendering a simple app component

---
+++ imports/ui/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class App extends Component {
+    render() {
+	return (
+	    <div>
+	      <p>This is some text</p>
+	    </div>
+	);
+    }
+}
+
+
